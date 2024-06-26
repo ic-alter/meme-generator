1663e02d58d24e561c30f5cae773e1f69c12124d
ic_alter <1600224799@qq.com>
2024-06-25 22:06:32 -0400

    init my fork

---
+++ start.sh
@@ -0,0 +1,14 @@
+docker run -d \
+  --name=meme-generator \
+  -p 2233:2233 \
+  --restart always \
+  -v /home/ubuntu/byy/bot/meme-generator/meme_generator:/app/meme_generator \
+  -v /home/ubuntu/byy/bot/meme-generator/meme-generator-contrib:/data \
+  -e MEME_DIRS='["/data/memes"]' \
+  -e MEME_DISABLED_LIST='[]' \
+  -e GIF_MAX_SIZE=10.0 \
+  -e GIF_MAX_FRAMES=100 \
+  -e BAIDU_TRANS_APPID='' \
+  -e BAIDU_TRANS_APIKEY='' \
+  -e LOG_LEVEL='INFO' \
+  meetwq/meme-generator:main
