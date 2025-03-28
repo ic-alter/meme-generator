fe33ff636de941d03713a401b224332b21082594
ic_alter <1600224799@qq.com>
2025-03-28 02:34:19 -0400

    ?

--- start.sh
+++ start.sh
@@ -4,12 +4,13 @@ docker run -d \
   --restart always \
   -v /home/ubuntu/byy/bot/meme-generator/meme_generator:/app/meme_generator \
   -v /home/ubuntu/byy/bot/meme-generator/meme-generator-contrib:/data \
-  -e MEME_DIRS='["/data/memes"]' \
+  -v /home/ubuntu/byy/bot/meme-generator/meme_emoji:/meme_emoji \
+  -e MEME_DIRS='["/data/memes","/meme_emoji/aircraft_cup","/meme_emoji/emoji"]' \
   -e MEME_DISABLED_LIST='[]' \
   -e GIF_MAX_SIZE=10.0 \
   -e GIF_MAX_FRAMES=100 \
-  -e BAIDU_TRANS_APPID='' \
-  -e BAIDU_TRANS_APIKEY='' \
+  -e BAIDU_TRANS_APPID='20250328002317685' \
+  -e BAIDU_TRANS_APIKEY='hu_ZbpCDuTtvwRh3OwWb' \
   -e LOG_LEVEL='INFO' \
   meetwq/meme-generator:main
 
